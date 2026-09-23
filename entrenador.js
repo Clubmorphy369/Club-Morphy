fca3cce2810f7eb9964b709c44c8ba60e778cc03
German Hernández Cornejo <clubmorphy369@gmail.com>
2026-09-23 01:06:56 -0600

    Update Stockfish worker URL path

--- entrenador.js
+++ entrenador.js
@@ -211,7 +211,7 @@
             if (this.worker || this.inicializado) return;
             this.inicializado = true;
             const url = window.stockfishWorkerUrl ||
-                '/assets/stockfish.js';
+                '/assets/stockfish/stockfish.js';
             try {
                 this.worker = new Worker(url);
                 this.worker.addEventListener('message', (e) => {
