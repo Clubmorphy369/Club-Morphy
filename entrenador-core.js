92e09dd1fc7e47798ac5033b12c75161b4d1f09e
German Hernández Cornejo <clubmorphy369@gmail.com>
2026-09-25 14:04:14 -0600

    Update entrenador-core.js

--- entrenador-core.js
+++ entrenador-core.js
@@ -202,7 +202,7 @@
             if (this.worker || this.inicializado) return;
             this.inicializado = true;
             const url = window.stockfishWorkerUrl ||
-                '/assets/stockfish/stockfish.js';
+            '/assets/stockfish/stockfish.wasm.js';
             try {
                 this.worker = new Worker(url);
                 this.worker.addEventListener('message', (e) => {
