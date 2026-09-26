d6ddb36c928b5ad5726a25bdec64f37ce66d110c
German Hernández Cornejo <clubmorphy369@gmail.com>
2026-09-26 13:01:14 -0600

    Update CACHE_NAME to version 45

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@
 // =============================================================
 
 // ⚠️ Sube la versión cuando hagas cambios importantes
-// ⭐ v44: fix ignoredHosts + unificar STATIC_ASSETS + añadir Lozza
-const CACHE_NAME = 'club-morphy-v44';
+// ⭐ v45: fix ignoredHosts + unificar STATIC_ASSETS + añadir Lozza
+const CACHE_NAME = 'club-morphy-v45';
 
 // Scope dinámico: funciona tanto en "/" (Firebase) como en "/Club-Morphy/" (GitHub Pages)
 const ROOT = self.registration.scope;
